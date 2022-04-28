e7da68b1f4b488b96e6a094782dbcfe8ac77bd44
Henning Pohl <HenningPohl@gmail.com>
2022-04-28 22:06:20 +0200

    Package initialized

---
+++ R/hcidata-package.R
@@ -0,0 +1,10 @@
+#' @name hcidata-package
+#' @aliases hcidata
+#' @docType package
+#' @importFrom Rdpack reprompt
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
